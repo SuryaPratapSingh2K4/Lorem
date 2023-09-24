37e62e9ac8cd162c0d7d858627d80baad93eca3e
surya pratap singh <surya2pratap0singh04@gmail.com>
2023-09-24 16:25:56 +0530

    first commit

---
+++ lib/pages/profilepage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20,),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                  "Lorem",
+                  style : TextStyle(
+                    fontSize : 24,
+                    fontFamily : 'R.font.poppins',
+                    fontWeight : FontWeight.w700,
+                    color : Color(0xFFE29D34),
+                  )),
+                  Icon(Icons.notification_add_rounded)
+            ],
+          ),
+          SizedBox(
+            height: 22,
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
